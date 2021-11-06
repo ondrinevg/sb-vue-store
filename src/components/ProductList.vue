29bac29fb46288c6f4a2c6a6387ec999d6f4e3c2
Eugene <ondrinevg@gmail.com>
2021-11-06 13:25:08 +0300

    start creating store

---
+++ src/components/ProductList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="catalog__list">
+    <ProductItem
+      :product="product"
+      v-for="product in products"
+      :key="product.title"
+    />
+  </ul>
+</template>
+
+<script>
+import ProductItem from "./ProductItem.vue";
+
+export default {
+  components: {
+    ProductItem,
+  },
+  name: "ProductList",
+  props: ["products"],
+  data() {
+    return {};
+  },
+};
+</script>
